e3b46a50a9ef3f6b4d872dbfb8c32f8021ed09f6
Taylor Miley <coachtaylormiley@gmail.com>
2015-08-24 13:29:57 -0500

    initial commit

---
+++ javascripts/dependencies.js
@@ -0,0 +1,6 @@
+define(function(require) {
+  //modules required in all other modules
+  var bootstrap = require("bootstrap"),
+      hbs = require("hbs"),
+      firebase = require("firebase");
+});
---
+++ javascripts/main.js
@@ -0,0 +1,25 @@
+requirejs.config({
+  baseUrl: "./javascripts",
+  paths: {
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "firebase": "../lib/bower_components/firebase/firebase",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
+    "q": "../lib/bower_components/q/q"
+  },
+  shim: {
+    "bootstrap": ["jquery"], //makes sure jquery is loaded before bootstrap
+    "firebase": {
+      exports: "Firebase"
+    }
+  }
+});
+
+//dependencies are global dependencies
+requirejs(["dependencies"], 
+  function(dependencies) {
+  
+  
+
+});
